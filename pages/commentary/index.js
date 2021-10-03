3348b90fc0eaedc2bd1502e42ea43f32880b509b
JoshHaokip <joshhaokip@gmail.com>
2021-10-03 16:54:30 +0100

    Meta D: Added MD for both About and Project pages

---
+++ pages/commentary/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function about() {
+  return (
+    <div>
+      <main></main>
+    </div>
+  );
+}
