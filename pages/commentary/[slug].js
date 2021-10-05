eeda57175e955bfbd0bdc156ee3149561c8db01d
JoshHaokip <joshhaokip@gmail.com>
2021-10-05 17:50:55 +0100

    Added incremental static regeneration

--- pages/commentary/[slug].js
+++ pages/commentary/[slug].js
@@ -28,6 +28,7 @@ export async function getStaticProps({ params }) {
     props: {
       joshBlog: res.items,
     },
+    revalidate: 30,
   };
 }
 
--- pages/commentary/index.js
+++ pages/commentary/index.js
@@ -18,6 +18,7 @@ export async function getStaticProps() {
     props: {
       joshBlog: res.items,
     },
+    revalidate: 30,
   };
 }
 
